99d774293922ed7e26c86c24e8e8ddeb760ec8f5
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-05-25 05:40:39 +0300

    project updated

--- web-forum/src/Card.js
+++ web-forum/src/Card.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Header from "./Header";
+import Body from "./Body";
 
-import ReactDOM from "react-dom";
+function Card(props) {
+    return (
+        <div>
+            <Header 
+            profileImg={props.commentObject.profileImg}
+            username={props.commentObject.username} />
 
+            <Body comment={props.commentObject.comment} />
 
+        </div>
+    )
+}
+
+export default Card
